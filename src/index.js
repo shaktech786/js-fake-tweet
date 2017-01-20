007125a0de5b811fb6695cc5a91363010fc11325
Shakeel Bhamani <sbhamani@thoughtworks.com>
2017-01-20 16:18:40 -0600

    adds webpack and makes postTweet a module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import TweetBox from "./TweetBox";
+import {postTweet} from "./postTweet";
+
 ReactDOM.render(
-  <TweetBox placeholder="What's up?" />,
+  <TweetBox placeholder="What's up?" postTweet={postTweet}/>,
   document.getElementById("root")
 );
